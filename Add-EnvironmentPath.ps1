c65d74d172a118a494f95369ced358d3d6085af8
Andreas Jagelund <andreas.jagelund@stenaline.io>
2021-01-20 15:05:43 +0100

    Resolve path before add to registry.

--- Add-EnvironmentPath.ps1
+++ Add-EnvironmentPath.ps1
@@ -9,8 +9,10 @@ if ((Test-Path -Path $Path) -eq $False) {
     Write-Error -Message "Path does not exist!" -ErrorAction Stop
 }
 
+$resolved = Resolve-Path $Path
+
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$newpath = "$oldpath;$Path"
+$newpath = "$oldpath;$resolved"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 
 (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# powershell-tools
+# Powershell Tools
 Misc powershell tools
+
+
+## Setup
+1. git clone repo
+2. Navigate to repo
+3. Add-EnvironmentPath.ps1 .
